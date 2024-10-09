2bf497f103165f6eb3ed234798cd9090fa4f23e8
ARTHUR KIPPER MIOTTO <arthur_miotto@estudante.sesisenai.org.br>
2024-10-09 08:54:25 -0300

    subindo novos arquivos

---
+++ app/index.jsx
@@ -0,0 +1,43 @@
+import { Text, View } from 'react-native'
+import {Link} from "expo-router"
+
+
+
+
+export default telaInicio = () => {
+    return (
+        <View>
+            <Link href='/lista-de-tarefas'>
+                <Text>Lista de Tarefas</Text>
+            </Link>
+            
+            <Link href= '/pokemon'>
+                <Text>Pokemon</Text>
+            </Link>
+                
+            <Link href= '/splash-screen'>
+                <Text>Splash Screen</Text>
+            </Link>
+               
+            <Link href= '/Santander'>
+                <Text>Santander</Text>
+            </Link>
+
+            <Link href= '/jogo-cartas'>
+            <Text>Jogo de cartas</Text>
+            </Link>
+
+            <Link href= '/Navegacao'>
+            <Text>Navegação entre telas</Text>
+            </Link>
+
+            <Link href= '/camera'>
+            <Text>Camera</Text>
+            </Link>
+
+            <Link href= '/galeria'>
+            <Text>Camera</Text>
+            </Link>
+        </View>
+    )
+}
